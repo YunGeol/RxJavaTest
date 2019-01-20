66af7ef419ad70a3f04a524e806c9528eaeef59f
YunGeol <lygirl@sk.com>
2019-01-20 15:08:49 +0900

    download initial project from start.spring.io

---
+++ src/main/java/yungeol/rxjavatest/RxjavatestApplication.java
@@ -0,0 +1,14 @@
+package yungeol.rxjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxjavatestApplication.class, args);
+	}
+
+}
+
